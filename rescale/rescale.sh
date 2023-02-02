ae2ddb2dee50c5b990ac56cdf0020d79f1f164cb
yeunkim <yeunkim@gmail.com>
2023-02-02 14:50:43 -0800

    rescale.sh corrected

--- rescale/rescale.sh
+++ rescale/rescale.sh
@@ -2,14 +2,15 @@
 
 ## Modify paths below
 dataDir=/path/to/studyDir
-rescale=/path/to/rescale.sh
+rescaledDataDir=/path/to/rescaledDataOutput
+rescale=/path/to/nii2int16.sh
 
 subjListText=$1
 
 readarray -t subjList < $subjListText
 
 for subj in ${subjList[@]}; do
-	 $rescale $dataDir/sub-${subj}/anat/sub-${subj}_T1w.nii.gz $dataDir/sub-${subj}/anat/sub-${subj}_T1w.nii.gz 
+	 $rescale $dataDir/sub-${subj}/anat/sub-${subj}_T1w.nii.gz $rescaledDataDir/sub-${subj}/anat/sub-${subj}_T1w.nii.gz
 done
 
 
