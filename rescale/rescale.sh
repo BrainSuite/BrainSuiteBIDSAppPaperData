88f22dbbaf215845fa8b96cc0290c4ad51c0f86c
yeunkim <yeunkim@gmail.com>
2023-02-02 16:30:19 -0800

    edited rescale.sh to use the participants.tsv file

--- rescale/rescale.sh
+++ rescale/rescale.sh
@@ -5,11 +5,10 @@ dataDir=/path/to/studyDir
 rescaledDataDir=/path/to/rescaledDataOutput
 rescale=/path/to/nii2int16.sh
 
-subjListText=$1
+subjListTSV=${dataDir}/participants.tsv
+subjList=( $(cut -f1 $subjListTSV) )
 
-readarray -t subjList < $subjListText
-
-for subj in ${subjList[@]}; do
+for subj in ${subjList[@]:1}; do
 	 $rescale $dataDir/sub-${subj}/anat/sub-${subj}_T1w.nii.gz $rescaledDataDir/sub-${subj}/anat/sub-${subj}_T1w.nii.gz
 done
 
