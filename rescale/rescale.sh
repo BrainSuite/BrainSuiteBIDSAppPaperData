6c6231017b23271838251377de8fc42b4c91fd88
yeunkim <yeunkim@gmail.com>
2023-02-02 17:45:19 -0800

    matlab script added for rescaling

--- rescale/rescale.sh
+++ rescale/rescale.sh
@@ -2,14 +2,13 @@
 
 ## Modify paths below
 dataDir=/path/to/studyDir
-rescaledDataDir=/path/to/rescaledDataOutput
 rescale=/path/to/nii2int16.sh
 
 subjListTSV=${dataDir}/participants.tsv
 subjList=( $(cut -f1 $subjListTSV) )
 
 for subj in ${subjList[@]:1}; do
-	 $rescale $dataDir/sub-${subj}/anat/sub-${subj}_T1w.nii.gz $rescaledDataDir/sub-${subj}/anat/sub-${subj}_T1w.nii.gz
+	 $rescale $dataDir/${subj}/anat/${subj}_T1w.nii.gz $dataDir/${subj}/anat/${subj}_T1w.nii.gz
 done
 
 
